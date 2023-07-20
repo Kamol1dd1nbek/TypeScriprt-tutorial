43b0cafc420f4c75950d73720b35fd8859b565a4
Kamoliddinbek <kamoliddinxudoyberdiyev72@gmail.com>
2023-07-20 19:31:13 +0500

    type

--- TypeScript tutorial/script.js
+++ TypeScript tutorial/script.js
@@ -1,2 +1,27 @@
-var name = "Sarvar";
-console.log(name);
+var firstname = "Sarvar"; // string
+var x = 9; // number
+// firstname = 9; // wrong
+var isMarried = false; // boolean
+var num = null; // null
+var a = undefined; // undefined
+// arrays
+var colors = ["yellow", "red"];
+// colors = ["sariq", 1, 2]; // wrong, only string
+var numbers = [1, 2, 3, 4]; // only numbers
+// any
+var smth = 1;
+smth = "qwer";
+smth = ["blue", 1];
+console.log(smth);
+// functions
+// void function dan hech qanday ma'lumot qaytmaydi
+function logger(a) {
+    if (typeof a == "number")
+        console.log(a.toFixed());
+    else if (typeof a == "string")
+        console.log(a.toLowerCase());
+}
+var logCar = function (car) {
+    return "Car name: ".concat(car.name, ", year: ").concat(car.year);
+};
+console.log(logCar({ name: "BMW", year: 2017 }));
--- TypeScript tutorial/script.ts
+++ TypeScript tutorial/script.ts
@@ -8,4 +8,34 @@ const isMarried: boolean = false; // boolean
 const num: null = null; // null
 const a: undefined = undefined; // undefined
 
-console.log(firstname);
+// arrays
+
+let colors: string[] = ["yellow", "red"];
+// colors = ["sariq", 1, 2]; // wrong, only string
+
+const numbers: number[] = [1, 2, 3, 4]; // only numbers
+
+// any
+
+let smth: any = 1;
+smth = "qwer";
+smth = ["blue", 1];
+console.log(smth);
+
+
+// functions
+
+// void function dan hech qanday ma'lumot qaytmaydi
+
+function logger(a: number | string): void{
+    if (typeof a == "number") console.log(a.toFixed());
+    else if (typeof a == "string") console.log(a.toLowerCase());
+}
+
+// year is not required
+const logCar = (car: {name: string, year: number}): string => {
+    return `Car name: ${car.name}, year: ${car.year}`;
+}
+
+console.log(logCar({name: "BMW", year: 2017}))
+;
